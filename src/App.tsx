01dd988cdb4ff0a43d77038be6b1d2aa81ade8d2
Jimmy <jimmystry@gmail.com>
2021-03-13 14:06:17 -0500

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p className="bg-gray-400">Hey!</p> 
+    <div className="App h-screen w-screen bg-gray-700">
+      <div>
+        <span className="text-9xl float-left ml-6">👐</span>
+        <h1 className='text-8xl pt-14 font-header'>Handy</h1>
+      </div>
     </div>
   );
 }
