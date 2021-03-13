b6ccab267b2635709a5be99dcbd7ffdec4b88277
Jimmy <jimmystry@gmail.com>
2021-03-13 17:07:39 -0500

    React router for secondary pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 import Header from './Header';
@@ -12,39 +17,51 @@ function App() {
       bg-gradient-to-r from-purple-800 to-purple-900'>
       </div>
 
-      <div>
-        <h2 className='text-center my-6 text-5xl'>Who are you?</h2>
+      <Router>
+        <Switch>
+          <Route path="/newissue">
+            <p>TODO:NEWISSUEPAGE</p>
+          </Route>
+          <Route path="/issues">
+            <p>TODO: SEARCH AND ISSUES LIST</p>
+          </Route>
+          <Route path="/">
+            <div>
+              <h2 className='text-center my-6 text-5xl'>Who are you?</h2>
 
-        <div className='grid grid-cols-2 content-evenly place-items-center text-center'>
-          <div className='landing-card'>
-            <h2 className='text-3xl'>
-              Project Manager
-            </h2>
-            <p className='w-2/3 text-lg'>
-              Do you need help on your open source projects, but they can't seem to gain any traction?
-              Upload your projects and issues to our platform, and we'll automatically
-              send bright coders your way!
-            </p>
-            <button className='button'>
-              Start Now!
-            </button>
-          </div>
+                <div className='grid grid-cols-2 content-evenly place-items-center text-center'>
+                <div className='landing-card'>
+                  <h2 className='text-3xl'>
+                    Project Manager
+                  </h2>
+                  <p className='w-2/3 text-lg'>
+                    Do you need help on your open source projects, but they can't seem to gain any traction?
+                    Upload your projects and issues to our platform, and we'll automatically
+                    send bright coders your way!
+                  </p>
+                  <button className='button'>
+                    <Link to="/newissue">Start Now!</Link>
+                  </button>
+                </div>
 
-          <div className='landing-card'>
-            <h2 className='text-3xl'>
-              Open Source Contributor
-            </h2>
-            <p className='w-2/3 text-lg'>
-              Do you want to contribute to open source software, but don't know
-              where to start? Don't know where you can help? Don't worry! Our system
-              will match you with the issues you're equipped to deal with.
-            </p>
-            <button className='button'>
-              Start Now!
-            </button>
-          </div>
-        </div>
-      </div>
+                <div className='landing-card'>
+                  <h2 className='text-3xl'>
+                    Open Source Contributor
+                  </h2>
+                  <p className='w-2/3 text-lg'>
+                    Do you want to contribute to open source software, but don't know
+                    where to start? Don't know where you can help? Don't worry! Our system
+                    will match you with the issues you're equipped to deal with.
+                  </p>
+                  <button className='button'>
+                    <Link to="/issues">Start Now!</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
