87e4eededb2e99296902038de98e1680240ea390
Jimmy <jimmystry@gmail.com>
2021-03-13 18:13:58 -0500

    Link to go home

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,8 +4,10 @@ const Header: React.FC = () => {
     return (
         <div className='Header h-1/4 pb-0'>
             <div className='ml-6 w-1/5'>
-              <span className="text-9xl float-left">👐</span>
-              <h1 className='text-8xl pt-14 font-header'>Handy</h1>
+                <a href='/'>
+                    <span className="text-9xl float-left">👐</span>
+                    <h1 className='text-8xl pt-14 font-header'>Handy</h1>
+                </a>
                 <div className='h-2 mx-auto rounded
                 bg-gradient-to-r from-purple-800 to-purple-900'>
                 </div>
